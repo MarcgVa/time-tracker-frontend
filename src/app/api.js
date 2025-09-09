b304f353ec5b735a3263dc3978f9bd1fbff1e8b9
Marc Grupe <marc.grupe@gmail.com>
2025-09-09 15:00:09 -0400

    added skeletal files for each function

---
+++ src/app/api.js
@@ -0,0 +1,21 @@
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_URL,
+    prepareHeaders: (headers) => {
+      const token = window.sessionStorage.getItem("token");
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Auth", "Projects", "TimeEntries", "Invoices"],
+  endpoints: () => ({}),
+});

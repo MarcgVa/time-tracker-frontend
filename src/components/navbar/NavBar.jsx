392652c12a9f5883217a412e0d5cf045c90c5699
Marc Grupe <marc.grupe@gmail.com>
2025-09-12 12:58:29 -0400

    added logout button and functionality to navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,12 +1,17 @@
+import './navbar.css'
 import { NavLink } from "react-router-dom";
 import logo from '../../assets/img/logos/TimeTrackerLogo-bgDark-400.png'
+import { useDispatch } from "react-redux";
+import { logout } from '../../features/auth/authSlice';
 
-import './navbar.css'
 export default function NavBar() {
-
+  const dispatch = useDispatch();
   return (
     <header>
       <div className="nav">
+        <div className="nav-logo">
+          <img src={logo} alt="Application Logo" className='nav-img' />
+        </div>
         <div className="navbar">
           <ul className="nav-list">
             <li>
@@ -28,8 +33,11 @@ export default function NavBar() {
             </li>
           </ul>
 
-          <div className="nav-img">
-            <img src={logo} alt="Application Logo" />
+          <div>
+            <button
+              className="oval-button"
+              onClick={dispatch(logout)}
+            >Logout</button>
           </div>
         </div>
       </div>

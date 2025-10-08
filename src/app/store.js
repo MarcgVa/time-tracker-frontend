192276493efe10a5644ed18258e87a5052c3c8ab
Marc Grupe <marc.grupe@gmail.com>
2025-10-07 20:02:24 -0400

    added company slice

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./api";
 import authReducer from "../routes/auth/authSlice";
 import invoiceReducer from "../routes/invoices/invoiceSlice";
+import companyReducer from "../routes/company/companySlice";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth: authReducer,
     invoice: invoiceReducer,
+    company: companyReducer,
   },
   middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(api.middleware),
 });

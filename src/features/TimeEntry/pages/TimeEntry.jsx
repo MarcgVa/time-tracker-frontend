7bbeeffb766c901e4a3093240120f73256f5115e
Marc Grupe <marc.grupe@gmail.com>
2025-10-19 13:11:16 -0400

    fixed alignment on public pages and reaction on private pages, and invoice lists

--- src/features/TimeEntry/pages/TimeEntry.jsx
+++ src/features/TimeEntry/pages/TimeEntry.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import { StartTimeEntry } from '../../Projects/components/StartTImeEntry';
+import React, { useState } from "react";
+import { StartTimeEntry } from "../../Projects/components/StartTImeEntry";
+import Button from "../../Shared/components/Button";
 
 export const TimeEntry = () => {
+  const [start, setStart] = useState(false);
   return (
-    <div>
+    <div className="relative w-full h-full flex flex-col justify-start text-white">
       <div>
-        <StartTimeEntry projectId={id} />
+        <h1 className="mt-2 ml-2 text-white text-4xl font-bold tracking-widest ">
+          Time Tracker
+        </h1>
       </div>
+      <div className="mt-6 flex ">
+        <div>Timer Area</div>
+        <div className="flex flex-col justify-end">
+          <div className="flex justify-end">
+            <Button
+              title={start ? "Stop" : "Start"}
+              className={`px-2 justify-end${
+                start ? "bg-red-950" : "bg-blue-500"
+              }`}
+            />
+          </div>
+          <div className="flex">
+            Project and Task Area
+
+          </div>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
-}
+};
